e53fb95ced1eb81c84e5295357b57eea643f9ae8
Fran <fuschendel@gmail.com>
2023-12-11 15:29:50 -0300

    Ultimos cambios

--- js/header.js
+++ js/header.js
@@ -1,13 +1,15 @@
-header =document.getElementById("header")
+header = document.getElementById("header")
 header.innerHTML = `<nav>
                         <ul>
                             <li><a href="#"><img src="./img/Filmopolis.png" alt="logo-FilmoPolis" width="120"></a></li>
-                            <li><a href="peliculas.html"><i class='bx bx-movie icono'></i>Catalogo</a></li>
-                            <li><span href="index.html" class="logout"><i class='bx bx-log-out'></i>Log out</span></li>
+                            <li class="pelis"><a href="peliculas.html"><i class='bx bx-movie icono'></i>Lista Peliculas</a></li>
+                            <li><span href="index.html" class="logout" id="logout"><i class='bx bx-log-out'></i>Log out</span></li>
                         </ul>
                     </nav>`
 
-header.addEventListener("click", ()=>{
+logout = document.getElementById("logout")
+
+logout.addEventListener("click", ()=>{
     localStorage.clear()
     window.location.href="index.html"
 })
