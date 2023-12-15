5992c52ceafc4c5d71c6c15da4e0a85917b8eae1
Fran <fuschendel@gmail.com>
2023-12-15 18:46:30 -0300

    Responsive Header

--- js/header.js
+++ js/header.js
@@ -1,15 +1,38 @@
 header = document.getElementById("header")
 header.innerHTML = `<nav>
-                        <ul>
+                        <ul class="menu-normal">
                             <li><a href="#"><img src="./img/Filmopolis.png" alt="logo-FilmoPolis" width="120"></a></li>
                             <li class="pelis"><a href="peliculas.html"><i class='bx bx-movie icono'></i>Lista Peliculas</a></li>
                             <li><span href="index.html" class="logout" id="logout"><i class='bx bx-log-out'></i>Log out</span></li>
                         </ul>
+                        <ul class="menu-hamburguesa">
+                            <li><a href="#"><img src="./img/Filmopolis.png" alt="logo-FilmoPolis" width="120"></a></li>
+                            <li id="hamburguesa"><i class='bx bx-menu icono hamburguesa'></i></li>
+                        </ul>
                     </nav>`
+
+
 
 logout = document.getElementById("logout")
 
 logout.addEventListener("click", ()=>{
     localStorage.clear()
     window.location.href="index.html"
+})
+
+hamburguesa = document.getElementById("hamburguesa")
+let menu_visible = false
+
+hamburguesa.addEventListener("click", ()=>{
+    menu_visible = !menu_visible
+    if(menu_visible) {
+        hamburguesa.innerHTML = "<i class='bx bx-menu icono hamburguesa'></i>"
+    } else{ 
+        hamburguesa.innerHTML += `<div class="contenido-hamburguesa">
+                                <ul>
+                                    <li><a href="peliculas.html"><i class='bx bx-movie icono'></i>Lista Peliculas</a></li>
+                                    <li><span href="index.html" class="logout" id="logout"><i class='bx bx-log-out'></i>Log out</span></li>
+                                </ul>
+                                </div>`
+    }
 })
